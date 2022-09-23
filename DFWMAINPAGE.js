d43eedb5db85e033049e591a551226b32ab0656d
exa2022 <110428536+exa2022@users.noreply.github.com>
2022-09-22 19:44:20 -0500

    Update DFWMAINPAGE.js

--- DFWMAINPAGE.js
+++ DFWMAINPAGE.js
@@ -1,28 +1,92 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {ImageBackground, View, Text, StyleSheet, Pressable} from 'react-native';
 
+
+
+const image = { uri: "https://d368g9lw5ileu7.cloudfront.net/races/race78752-social1200x630.bFYauv.jpg" };
 
 const DFWSCREEN = ({navigation}) => {
     return (
-        <View style = {StyleSheet.container}>
-            <Text>
-                HomeScreen
+       
+        <View style = {styles.container}>
+            <Text style = {styles.Headline}>Welcome to the DFW Main Page!
+            {"\n"}
+            {"\n"}
+            </Text> 
+            <Text style = {styles.Text1}>A brighter future for children!
+            {"\n"}
+            {"\n"}
             </Text>
-            <Button
-                title = "Click here"
-                onPress={() => alert('Button clicked')}
-            />
+            <Text style = {styles.Text2}>Click HERE for current events details and HERE to visit the external registration page
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            </Text>
+            <Pressable style = {styles.ButtonContainer}>
+                <Text style = {styles.ButtonText}>Share</Text>
+            </Pressable>
         </View>
+     
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
+      flex: 5,
+      backgroundColor: 'lightblue',
       alignItems: 'center',
       justifyContent: 'center',
+      marginBottom: 100,
     },
+    Headline: {
+        fontSize: 60,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginTop: 130,
+    },
+    Text1: {
+        fontSize: 30,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'purple',
+        fontStyle: 'italic',
+        marginTop: 20,
+    },
+    Text2: {
+        fontSize: 25,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    ButtonContainer: {
+        backgroundColor: 'orange',
+        height: 45,
+        width: 170,
+        justifyContent: 'center',
+        marginHorizontal: 930,
+        marginTop: 20,
+        marginBottom: 190,
+        borderColor: 'white',
+        borderWidth: 4,
+        
+    },
+    ButtonText: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    image: {
+        height: 100,
+        width: 700,
+    }
   });
 
 
